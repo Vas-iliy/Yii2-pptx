1e251b5a3e8b57100927d2f2c79f5e662f35fc32
Vasiliy <kkolyasev1228@gmail.com>
2022-03-03 16:08:05 +0400

    new setting files

--- config/params.php
+++ config/params.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'directoryPptx' => Yii::getAlias('@web/files/pptx'),
+    'directoryTmpFiles' => Yii::getAlias('@web/files/tmp'),
+    'directoryImages' => Yii::getAlias('@web/files/images'),
+    'directoryPdf' => Yii::getAlias('@web/files/pdf'),
 ];
---
+++ migrations/m220303_120244_create_files_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%files}}`.
+ */
+class m220303_120244_create_files_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%files}}', [
+            'id' => $this->primaryKey(),
+            'pdf' => $this->string(),
+            'image' => $this->string()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%files}}');
+    }
+}
